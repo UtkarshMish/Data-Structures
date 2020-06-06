c3b27360e187c392012d59a271040d9b1b76c6b5
UtkarshMish <iiutkarshmishraii@gmail.com>
2020-06-06 07:32:25 +0530

    added directory cmakelists

--- SparseMatrix/main.cpp
+++ SparseMatrix/main.cpp
@@ -1,59 +1,68 @@
 #include "iostream"
 using namespace std;
 template <class T>
-class items{
+class items
+{
 public:
-     T x;
-     T y;
-     T value;
+    T x;
+    T y;
+    T value;
 };
-class sparse{
+class sparse
+{
 private:
-    int m,n,size;
-    items<int>  *Elements;
+    int m, n, size;
+    items<int> *Elements;
+
 public:
-    sparse(int i,int j, int size){
+    sparse(int i, int j, int size)
+    {
         this->m = i;
         this->n = j;
         this->size = size;
-        Elements = new items<int> [size];
-
+        Elements = new items<int>[size];
     }
-    void getElements(){
-        for (int i=0;i<size;i++) {
-            cout <<i+1<< ") Enter i, j : ";
+    void getElements()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << i + 1 << ") Enter i, j : ";
             cin >> Elements[i].x >> Elements[i].y;
-            cout<<endl<<"Enter Value:" ;
-            cin>> Elements[i].value;
-            cout<<endl;
-        }
-
-
-    }
-    void show(){
-        cout<<m<<"      "<<n<<"      "<<size<<endl;
-            for(int i=0;i<size;i++){
-                cout<<Elements[i].x<<"      "<<Elements[i].y<<"      "<<Elements[i].value;
-                cout<< endl;
+            cout << endl
+                 << "Enter Value:";
+            cin >> Elements[i].value;
+            cout << endl;
         }
     }
-    ~sparse(){
+    void show()
+    {
+        cout << m << "      " << n << "      " << size << endl;
+        for (int i = 0; i < size; i++)
+        {
+            cout << Elements[i].x << "      " << Elements[i].y << "      " << Elements[i].value;
+            cout << endl;
+        }
+    }
+    ~sparse()
+    {
         free(Elements);
     }
-
 };
 int main()
-{   int i,j,size;
-    cout<<"Enter dimensions i and j: ";
-    cin>>i>>j;
-    cout<<endl;
-    cout<<"Enter total number of element: ";
-    cin>>size;
-    cout<<endl;
-    if( i*j < size){
-        cout<<"Matrix not valid !!!"<<endl;
+{
+    int i, j, size;
+    cout << "Enter dimensions i and j: ";
+    cin >> i >> j;
+    cout << endl;
+    cout << "Enter total number of element: ";
+    cin >> size;
+    cout << endl;
+    if (i * j < size)
+    {
+        cout << "Matrix not valid !!!" << endl;
     }
-    else {
+    else
+    {
         auto matrix = new sparse(i, j, size);
         matrix->getElements();
         matrix->show();
--- SpecialMatrix/main.cpp
+++ SpecialMatrix/main.cpp
@@ -2,177 +2,214 @@
 #include "map"
 using namespace std;
 
-class customMatrix{
+class customMatrix
+{
 private:
-    int *matrix;
+	int *matrix;
+
 public:
-    int size;
-    int TYPE;
-    map<int, string> matrixTypes ={
-                            {1,"diagonal"},
-                            {2,"lower-triangular"},
-                            {3,"upper-triangular"},
-                            {4,"symmetric"},
-                            {5,"toeplitz"}
-                              };
-    customMatrix(int sz, int TYPE){
-        size = sz;
-        this->TYPE = TYPE;
-        switch(TYPE){
-            case 1:
-                matrix = new int [sz];
-                break;
-            case 2:
-            case 3:
-            case 4:
-                matrix = new int [(sz*(sz+1)/2)];
-                break;
-            case 5:
-                matrix = new int [(2*sz - 1)];
-                break;
-            default:
-                cout<<"Invalid CHOICE Try Again Later !!!"<<endl;
-                exit(0);
-        }
-        cout<<"TYPE IS "<<matrixTypes[TYPE]<<endl;
-    }
-    void getElements(){
-        switch(TYPE){
-            case 1:
-                for (int i=0; i < size; i++){
-                    cout<<"Enter Element matrix["<<i+1<<"]"<<"["<<i+1<<"]: ";
-                    cin>>matrix[i];
-                }
-                break;
-            case 2:
+	int size;
+	int TYPE;
+	map<int, string> matrixTypes = {
+			{1, "diagonal"},
+			{2, "lower-triangular"},
+			{3, "upper-triangular"},
+			{4, "symmetric"},
+			{5, "toeplitz"}};
+	customMatrix(int sz, int TYPE)
+	{
+		size = sz;
+		this->TYPE = TYPE;
+		switch (TYPE)
+		{
+		case 1:
+			matrix = new int[sz];
+			break;
+		case 2:
+		case 3:
+		case 4:
+			matrix = new int[(sz * (sz + 1) / 2)];
+			break;
+		case 5:
+			matrix = new int[(2 * sz - 1)];
+			break;
+		default:
+			cout << "Invalid CHOICE Try Again Later !!!" << endl;
+			exit(0);
+		}
+		cout << "TYPE IS " << matrixTypes[TYPE] << endl;
+	}
+	void getElements()
+	{
+		switch (TYPE)
+		{
+		case 1:
+			for (int i = 0; i < size; i++)
+			{
+				cout << "Enter Element matrix[" << i + 1 << "]"
+						 << "[" << i + 1 << "]: ";
+				cin >> matrix[i];
+			}
+			break;
+		case 2:
 
-                for(int i=0,k=0; i<size;i++){
-                    for(int j = 0;j<size;j++)
-                        if(j<=i) {
-                            cout << "Enter Element matrix[" << i + 1 << "]" << "[" << j + 1 << "]: ";
-                            cin >> matrix[k];
-                            k++;
-                        }
-                }
-                break;
-            case 3:
-                for(int i=0,k=0; i<size;i++){
-                    for(int j = 0;j<size;j++)
-                        if(i<=j) {
-                            cout << "Enter Element matrix[" << i + 1 << "]" << "[" << j + 1 << "]: ";
-                            cin >> matrix[k];
-                            k++;
-                        }
-                }
-                break;
-            case 4:
-                for(int i=0,k=0; i<size;i++){
-                    for(int j = 0;j<size;j++)
-                        if(j<=i) {
-                            cout << "Enter Element matrix[" << i + 1 << "]" << "[" << j + 1 << "]: ";
-                            cin >> matrix[k];
-                            k++;
-                        }
-                        else break;
-                }
-                break;
-            case 5:
-                int k=0;
-                for (int i = 0; i< size ; i++,k++){
-                    cout << "Enter Element matrix[" << 1 << "]" << "[" <<  i+1 << "]: ";
-                    cin >> matrix[k];
-                }
-                for (int i = 1; i< size ; i++,k++){
-                    cout << "Enter Element matrix[" << i+1 << "]" << "[" <<  1 << "]: ";
-                    cin >> matrix[k];
-                }
-                break;
-        }
+			for (int i = 0, k = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+					if (j <= i)
+					{
+						cout << "Enter Element matrix[" << i + 1 << "]"
+								 << "[" << j + 1 << "]: ";
+						cin >> matrix[k];
+						k++;
+					}
+			}
+			break;
+		case 3:
+			for (int i = 0, k = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+					if (i <= j)
+					{
+						cout << "Enter Element matrix[" << i + 1 << "]"
+								 << "[" << j + 1 << "]: ";
+						cin >> matrix[k];
+						k++;
+					}
+			}
+			break;
+		case 4:
+			for (int i = 0, k = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+					if (j <= i)
+					{
+						cout << "Enter Element matrix[" << i + 1 << "]"
+								 << "[" << j + 1 << "]: ";
+						cin >> matrix[k];
+						k++;
+					}
+					else
+						break;
+			}
+			break;
+		case 5:
+			int k = 0;
+			for (int i = 0; i < size; i++, k++)
+			{
+				cout << "Enter Element matrix[" << 1 << "]"
+						 << "[" << i + 1 << "]: ";
+				cin >> matrix[k];
+			}
+			for (int i = 1; i < size; i++, k++)
+			{
+				cout << "Enter Element matrix[" << i + 1 << "]"
+						 << "[" << 1 << "]: ";
+				cin >> matrix[k];
+			}
+			break;
+		}
 
-        cout<<"Matrix Successfully Saved ."<<endl;
-    }
-    void DisplayMatrix(){
-        switch(TYPE){
-            case 1:
-                for(int i=0;i<size;i++){
-                    for(int j=0;j<size;j++) {
-                        if (i == j)
-                            cout << matrix[i] << "      ";
-                        else
-                            cout << 0 << "      ";
-                    }
-                    cout<<endl;
-                }
-                break;
-            case 2:
-                for (int i=0,k=0; i<size;i++){
-                    for(int j=0;j<size;j++){
-                        if(j<=i){
-                            cout<<matrix[k]<<"      ";
-                            k++;
-                        }
-                        else{
-                            cout<<0<<"      ";
-                        }
-                    }
-                    cout<<endl;
-                }
-                break;
-            case 3:
-                for (int i=0,k=0; i<size;i++){
-                    for(int j=0;j<size;j++){
-                        if(i<=j){
-                            cout<<matrix[k]<<"      ";
-                            k++;
-                        }
-                        else{
-                            cout<<0<<"      ";
-                        }
-                    }
-                    cout<<endl;
-                }
-                break;
-            case 4:
-                for (int i=1; i<=size;i++){
-                    for(int j=1;j<=size;j++) {
-                                cout<<matrix[(i*(i-1))/2+j - 1]<<"        ";
-                    }
-                    cout<<endl;
-                }
-                break;
-            case 5:
-                for (int i=0; i<size;i++){
-                    for(int j=0;j<size;j++) {
-                        if( i<=j)
-                            cout<<matrix[j-i]<<"     ";
-                        else if (j< i)
-                            cout<<matrix[((size) + i-j) - 1]<<"     ";
+		cout << "Matrix Successfully Saved ." << endl;
+	}
+	void DisplayMatrix()
+	{
+		switch (TYPE)
+		{
+		case 1:
+			for (int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					if (i == j)
+						cout << matrix[i] << "      ";
+					else
+						cout << 0 << "      ";
+				}
+				cout << endl;
+			}
+			break;
+		case 2:
+			for (int i = 0, k = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					if (j <= i)
+					{
+						cout << matrix[k] << "      ";
+						k++;
+					}
+					else
+					{
+						cout << 0 << "      ";
+					}
+				}
+				cout << endl;
+			}
+			break;
+		case 3:
+			for (int i = 0, k = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					if (i <= j)
+					{
+						cout << matrix[k] << "      ";
+						k++;
+					}
+					else
+					{
+						cout << 0 << "      ";
+					}
+				}
+				cout << endl;
+			}
+			break;
+		case 4:
+			for (int i = 1; i <= size; i++)
+			{
+				for (int j = 1; j <= size; j++)
+				{
+					cout << matrix[(i * (i - 1)) / 2 + j - 1] << "        ";
+				}
+				cout << endl;
+			}
+			break;
+		case 5:
+			for (int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					if (i <= j)
+						cout << matrix[j - i] << "     ";
+					else if (j < i)
+						cout << matrix[((size) + i - j) - 1] << "     ";
+				}
+				cout << endl;
+			}
+			break;
+		default:
+			cout << "Invalid CHOICE Try Again Later !!!" << endl;
+			exit(0);
+		}
 
-                    }
-                    cout<<endl;
-                }
-                break;
-            default:
-                cout<<"Invalid CHOICE Try Again Later !!!"<<endl;
-                exit(0);
-        }
+		cout << endl;
+	}
 
-        cout<<endl;
-    }
-
-    ~customMatrix(){
-        free(matrix);
-    }
+	~customMatrix()
+	{
+		free(matrix);
+	}
 };
-int main() {
-    int n;
-    cout << "Enter Size of Matrix: " << endl;
-    cin>>n;
+int main()
+{
+	int n;
+	cout << "Enter Size of Matrix: " << endl;
+	cin >> n;
 
-
-    customMatrix mtrx =  customMatrix(n,5);
-    mtrx.getElements();
-    mtrx.DisplayMatrix();
-    cout<<"Dimension: "<<mtrx.size<<"*"<<mtrx.size;
-    return 0;
+	customMatrix mtrx = customMatrix(n, 5);
+	mtrx.getElements();
+	mtrx.DisplayMatrix();
+	cout << "Dimension: " << mtrx.size << "*" << mtrx.size;
+	return 0;
 }
