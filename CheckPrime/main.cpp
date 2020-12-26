15425704d7a0f1a665e39bd9ca2e9f370b8a835b
UtkarshMish <iiutkarshmishraii@gmail.com>
2020-12-26 08:52:43 +0530

    refactored code

--- CheckPrime/main.cpp
+++ CheckPrime/main.cpp
@@ -1,27 +1,35 @@
 #include "iostream"
 #include <cmath>
 using namespace std;
-string checkPrime(long num){
-    const string result[] = {"Not Prime", "Prime"};
-    if(num == 0 || num == 1)
-        return result[1];
-    else if(num%2 == 0)
-        return result[0];
-    float n = sqrt(num);
-    if ((int)ceil(n) == (int)n)
-        return result[0];
-    else {
-        n = ceil(n);
-        for (long i = 3; i < n ; i+=2) {
-            if (num % i == 0)
-                return result[0];
+bool checkPrime(long num)
+{
+    if (num == 0 || num == 1)
+        return false;
+    else if (num == 2)
+        return true;
+    else if (num % 2 == 0)
+        return false;
+    else
+    {
+        float n = sqrt(num);
+        if ((int)ceil(n) == (int)n)
+            return false;
+        else
+        {
+            n = ceil(n);
+            for (long i = 3; i < n; i += 2)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
         }
-        return result[1];
     }
 }
-int main(){
+int main()
+{
     long num;
-    cout<<"Enter a number to check if prime: ";
-    cin>>num;
-    cout<<checkPrime(num);
+    cout << "Enter a number to check if prime: ";
+    cin >> num;
+    cout << (checkPrime(num) ? "Prime" : "Not Prime");
 }
